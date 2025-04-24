cbfaad16cb029a67a34c28ef74be281d9cdc7733
Di3go0-0 <d.rincon@utp.edu.co>
2025-04-23 21:17:35 -0500

    update primsa config

--- api/src/shared/shared.module.ts
+++ api/src/shared/shared.module.ts
@@ -5,7 +5,11 @@ import { JwtGuardModule } from './jwt-guard/jwt-guard.module';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [JwtModule, JwtGuardModule, PrismaModule],
+  imports: [
+    JwtModule,
+    JwtGuardModule,
+    PrismaModule
+  ],
   providers: [JwtGuardService]
 })
 export class SharedModule { }
