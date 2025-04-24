18849d571397ecd55551c83f4b786218a4f73c75
Di3go0-0 <d.rincon@utp.edu.co>
2025-04-23 20:18:58 -0500

    fix: move prisma to share modules

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,14 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ModulesModule } from './modules/modules.module';
 import { ConfigModule } from '@nestjs/config';
-import { PrismaModule } from './prisma/prisma.module';
 import { SharedModule } from './shared/shared.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, }),
     ModulesModule,
-    PrismaModule,
     SharedModule
   ],
   providers: [],
--- api/src/shared/shared.module.ts
+++ api/src/shared/shared.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { JwtModule } from './jwt/jwt.module';
 import { JwtGuardService } from './jwt-guard/jwt-guard.service';
 import { JwtGuardModule } from './jwt-guard/jwt-guard.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [JwtModule, JwtGuardModule],
+  imports: [JwtModule, JwtGuardModule, PrismaModule],
   providers: [JwtGuardService]
 })
 export class SharedModule { }
