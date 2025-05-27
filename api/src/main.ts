e2f4bc87047dc5f16a49fff625ded7970680e5f0
Di3go0-0 <d.rincon@utp.edu.co>
2025-05-26 21:54:14 -0500

    update env render

--- api/src/main.ts
+++ api/src/main.ts
@@ -7,7 +7,8 @@ import { SwaggerConfig } from './shared/swagger/swagger.config';
 
 async function bootstrap() {
   const rute = ENV.CONSTANS.SWAGGER_RUTE;
-  const port = ENV.CONSTANS.API_PORT || 3001;
+  const port = process.env.PORT || ENV.CONSTANS.API_PORT || 3001;
+
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(
@@ -26,9 +27,10 @@ async function bootstrap() {
       deepScanRoutes: true,
     }),
   )
-
   console.log(`Swagger running on: localhost:${port}/${rute}`);
-  app.enableCors(); // <-- Esto es clave
+
+  app.enableCors();
+
   await app.listen(port, '0.0.0.0');
 }
 bootstrap();
