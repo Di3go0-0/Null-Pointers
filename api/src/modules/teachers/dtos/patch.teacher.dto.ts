f0fe7ced0bcc3a9cf3ad23a924e005cf15ea7298
Di3go0-0 <d.rincon@utp.edu.co>
2025-06-06 20:20:00 -0500

    fix: update Teacher

--- api/src/modules/teachers/dtos/patch.teacher.dto.ts
+++ api/src/modules/teachers/dtos/patch.teacher.dto.ts
@@ -1,25 +1,26 @@
 import { ApiProperty, PartialType } from "@nestjs/swagger";
 import { BaseTeacherDTO } from "./base.teacher.dto";
-import { IsNotEmpty, IsString, IsEmail } from "class-validator";
+import { IsNotEmpty, IsString, IsEmail, IsOptional } from "class-validator";
 
 export class PatchTeacherDTO extends PartialType(BaseTeacherDTO) {
   @ApiProperty({
     type: String,
     example: 'Pepito',
-    required: true,
+    required: false,
     description: 'Nombre del usuario',
   })
-  @IsNotEmpty({ message: 'El nombre es requerido' })
+  @IsOptional()
   @IsString({ message: 'El nombre debe ser un texto' })
-  name: string;
+  name?: string;
 
   @ApiProperty({
     type: String,
     example: 'usuario@correo.com',
-    required: true,
+    required: false,
     description: 'Correo electrónico del usuario',
   })
-  @IsNotEmpty({ message: 'El email es requerido' })
+  @IsOptional()
+  @IsString({ message: 'El nombre debe ser un texto' })
   @IsEmail({}, { message: 'Debe ingresar un email válido' })
-  email: string;
+  email?: string;
 }
