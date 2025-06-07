b27b837b3867a6c175880c3fdc83e58115d3ea1f
Di3go0-0 <d.rincon@utp.edu.co>
2025-06-06 22:32:49 -0500

    Feature: grades update dto

--- api/src/modules/grades-extension-courses/grades-extension-courses.controller.ts
+++ api/src/modules/grades-extension-courses/grades-extension-courses.controller.ts
@@ -1,9 +1,9 @@
 import { Body, Controller, Get, Param, ParseIntPipe, Patch, Post, Query } from '@nestjs/common';
 import { GradesCoursesService } from './grades-extension-courses.service';
-import { PatchGradesDto } from './dtos/patch.grades.dto';
-import { PostGradesDto } from './dtos/post.grades.dto';
+import { PostExtensionGradesDto } from './dtos/post.grades.dto';
 import { GetGradeDto } from './dtos/get.grades.dto';
 import { ApiTags } from '@nestjs/swagger';
+import { PatchExtensionGradesDto } from './dtos/patch.grades.dto';
 
 @ApiTags('Extension Course Grades')
 @Controller('grades-extesnion-courses')
@@ -17,12 +17,12 @@ export class GradesExtensionCoursesController {
 
 
   @Post()
-  async postGrade(@Body() body: PostGradesDto) {
+  async postGrade(@Body() body: PostExtensionGradesDto) {
     return this.gradesCoursesService.postGrade(body);
   }
 
   @Patch('/:id')
-  async patchGrade(@Param('id', ParseIntPipe) id: number, @Body() body: PatchGradesDto) {
+  async patchGrade(@Param('id', ParseIntPipe) id: number, @Body() body: PatchExtensionGradesDto) {
     return this.gradesCoursesService.patchGrade(id, body);
   }
 }
